73df30aa9e92ecf488cd3a49da8eba77465c6433
Lisa Miner <midnightmoet@gmail.com>
2023-07-24 19:36:42 -0400

    Still playing with this one

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ This is a simple project. It's a rating system that uses emojis instead of stars
 - HTML5
 - CSS3
 - JavaScript
+- [Font Awesome](https://fontawesome.com/) for the icons used
+- [CDNJS](https://cdnjs.com/) for the Font Awesome CDN
---
+++ script.js
@@ -0,0 +1,39 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojisEl = document.querySelectorAll(".far");
+const colorsArray = ["red", "orange", "lightblue", "lightgreen", "green"];
+const btnEl = document.querySelector(".btn");
+
+updateRating(0);
+
+starsEl.forEach((starEl, index) => {
+  starEl.addEventListener("click", () => {
+    updateRating(index);
+  });
+});
+
+function updateRating(index) {
+  starsEl.forEach((starEl, idx) => {
+    if (idx < index + 1) {
+      starEl.classList.add("active");
+    } else {
+      starEl.classList.remove("active");
+    }
+  });
+
+  emojisEl.forEach((emojiEl) => {
+    emojiEl.style.transform = `translateX(-${index * 50}px)`;
+    emojiEl.style.color = colorsArray[index];
+  });
+}
+
+btnEl.addEventListener("click", () => {
+    const message = document.querySelector(".message");
+    message.innerHTML = `Thanks for rating! This will aid us in improving our services.`;
+
+    message.style.display = "block";
+    setTimeout(() => {
+        message.style.display = "none";
+    }
+    , 3000);
+
+});
